67d2303d95e0b3001226191fd8537d2a71ad7fb8
Jörg Herzinger <joerg.herzinger@global2000.at>
2012-11-08 18:47:14 +0100

    refine try_include_recipe

--- libraries/try_include_recipe.rb
+++ libraries/try_include_recipe.rb
@@ -1,7 +1,12 @@
-def try_include_recipe( recipe )
-  begin
-    include_recipe "#{recipe}"
-  rescue
-    Chef::Log.info("Loading recipe #{recipe} did not work, no worries here")
+class Chef
+  class Recipe
+    def try_include_recipe( recipe )
+      begin
+        include_recipe "#{recipe}"
+      rescue
+        Chef::Log.info("Loading recipe #{recipe} did not work, no worries here")
+      end
+    end
   end
 end
+
